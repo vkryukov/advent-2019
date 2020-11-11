1781d2373a4143fda93f3c424b74cdfaf09eb271
Victor <victor.kryukov@gmail.com>
2020-11-11 01:09:02 -0800

    Day 5, implement, read/write and pass test

--- day5.py
+++ day5.py
@@ -7,9 +7,11 @@ class Intcode:
     EXIT_CODE = 99
     ADD_CODE = 1
     MUL_CODE = 2
+    READ_INPUT_CODE = 3
+    WRITE_OUTPUT_CODE = 4
 
     def __init__(self, program):
-        self._program = program
+        self._program = program[:]
         self._memory = program[:]
 
     @staticmethod
@@ -24,10 +26,10 @@ class Intcode:
 
     def values(self, counter, count, modes):
         """Given the memory counter and params, return COUNT values according"""
-        return [self._memory[self._memory[counter+i]]
+        return [self._memory[self._memory[counter+i+1]]
                 if (i >= len(modes) or modes[i] == 0)
-                else self._memory[counter+i]
-                for i in range(1, count+1)]
+                else self._memory[counter+i+1]
+                for i in range(count)]
 
     def run(self, input: Optional[list] = None) -> list:
         """Runs Intcode on a given input, producing given output."""
@@ -45,6 +47,14 @@ class Intcode:
                 x, y = self.values(counter, 2, modes)
                 self._memory[self._memory[counter + 3]] = x * y
                 counter += 4
+            elif op == Intcode.READ_INPUT_CODE:
+                x, = self.values(counter, 1, modes)
+                self._memory[x] = input[input_counter]
+                input_counter += 1
+                counter += 2
+            elif op == Intcode.WRITE_OUTPUT_CODE:
+                x, = self.values(counter, 1, modes)
+                output.append(self._memory[x])
             else:
                 raise ValueError(f'Unexpected code {self._memory[counter]} at position {counter}.\n{self._memory=}')
         return output
@@ -55,7 +65,7 @@ class Intcode:
         return self._memory
 
 
-def test_intcode():
+def test_intcode_basic_program():
     for start_program, end_program in [
         [[1, 9, 10, 3, 2, 3, 11, 0, 99, 30, 40, 50],
          [3500, 9, 10, 70, 2, 3, 11, 0, 99, 30, 40, 50]],
@@ -71,3 +81,12 @@ def test_intcode():
         comp = Intcode(start_program)
         comp.run()
         assert comp.memory == end_program
+
+
+def test_another_simple_program():
+    comp = Intcode([1002, 4, 3, 4, 33])
+    comp.run()
+    assert comp.memory == [1002, 4, 3, 4, 99]
+    comp = Intcode([1101, 100, -1, 4, 0])
+    comp.run()
+    assert comp.memory == [1101, 100, -1, 4, 99]
