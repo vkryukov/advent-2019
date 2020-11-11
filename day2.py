95152253e5368a40e1af683dffdc23710abba8d5
Victor <victor.kryukov@gmail.com>
2020-11-11 12:19:14 -0800

    Use intcode computer in Day 2

--- day2.py
+++ day2.py
@@ -1,55 +1,30 @@
+from intcode import Intcode
+from utils import read_integers
 
-EXIT_CODE = 99
-ADD_CODE = 1
-MUL_CODE = 2
-
-def intcode(prog: list) -> list:
-    counter = 0
-    while prog[counter] != EXIT_CODE:
-        if prog[counter] == ADD_CODE:
-            prog[prog[counter + 3]] = prog[prog[counter + 1]] + prog[prog[counter + 2]]
-        elif prog[counter] == MUL_CODE:
-            prog[prog[counter + 3]] = prog[prog[counter + 1]] * prog[prog[counter + 2]]
-        else:
-            raise ValueError(f'Unexpected code {prog[counter]} at position {counter}.\n{prog=}')
-        counter += 4
-    return prog
+PROGRAM = read_integers('inputs/day2.txt')
 
 
-def test_intcode():
-    for start_program, end_program in [
-        [[1, 9, 10, 3, 2, 3, 11, 0, 99, 30, 40, 50],
-         [3500, 9, 10, 70, 2, 3, 11, 0, 99, 30, 40, 50]],
-        [[1, 0, 0, 0, 99],
-         [2, 0, 0, 0, 99]],
-        [[2, 3, 0, 3, 99],
-         [2, 3, 0, 6, 99]],
-        [[2, 4, 4, 5, 99, 0],
-         [2, 4, 4, 5, 99, 9801]],
-        [[1, 1, 1, 4, 99, 5, 6, 0, 99],
-         [30, 1, 1, 4, 2, 5, 6, 0, 99]],
-    ]:
-        assert intcode(start_program) == end_program
-
-
-def run_program(prog, noun, verb):
-    prog = prog[:]
+def run_program(noun, verb):
+    prog = PROGRAM[:]
     prog[1] = noun
     prog[2] = verb
-    return intcode(prog)[0]
+    comp = Intcode(prog)
+    comp.run()
+    return comp._memory[0]
 
 
-PROG = [int(x) for x in open('inputs/day2.txt').read().split(',')]
+def test_part1():
+    assert run_program(12, 2) == 10566835
 
 
-def test_run_program():
-    assert run_program(PROG, 12, 2) == 10566835
+def test_part2():
+    assert run_program(23, 47) == 19690720
 
 
 def main():
     for noun in range(100):
         for verb in range(100):
-            if run_program(PROG, noun, verb) == 19690720:
+            if run_program(noun, verb) == 19690720:
                 print(100 * noun + verb)
                 return
 
--- day5.py
+++ day5.py
@@ -1,6 +1,7 @@
 from utils import read_integers
 from intcode import Intcode
 
+
 def test_intcode_basic_program():
     for start_program, end_program in [
         [[1, 9, 10, 3, 2, 3, 11, 0, 99, 30, 40, 50],
