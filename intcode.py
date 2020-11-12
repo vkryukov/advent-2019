0f1d2fa3e6f6bcf31d301b7960d99cadf3bdac28
Victor <victor.kryukov@gmail.com>
2020-11-12 14:15:17 -0800

    Another refactoring

--- intcode.py
+++ intcode.py
@@ -20,7 +20,7 @@ class Intcode:
 
     def __init__(self, program):
         self._program = program[:]
-        self._memory = self._input = None
+        self._memory = self._input = self.pos = None
 
     @staticmethod
     def op_and_params(instruction):
@@ -32,15 +32,16 @@ class Intcode:
             instruction //= 10
         return op, modes
 
-    def values(self, counter, count, modes):
+    def values(self, count, modes):
         """Given the memory counter and params, return COUNT values according"""
-        return [(self._memory[self._memory[counter + i + 1]]
+        return [(self._memory[self._memory[self.pos + i + 1]]
                  if (i >= len(modes) or modes[i] == 0)
-                 else self._memory[counter + i + 1])
+                 else self._memory[self.pos + i + 1])
                 for i in range(count)]
 
-    def reset(self):
+    def reset(self, inp: Optional[list] = None):
         self._memory = self._program[:]
+        self._input = inp[:] if inp else []
         self.pos = 0
 
     def run_until_output(self) -> Optional[int]:
@@ -48,38 +49,38 @@ class Intcode:
         while self._memory[self.pos] != Intcode.EXIT:
             op, modes = Intcode.op_and_params(self._memory[self.pos])
             if op == Intcode.ADD:
-                x, y = self.values(self.pos, 2, modes)
+                x, y = self.values(2, modes)
                 self._memory[self._memory[self.pos + 3]] = x + y
                 self.pos += 4
             elif op == Intcode.MUL:
-                x, y = self.values(self.pos, 2, modes)
+                x, y = self.values(2, modes)
                 self._memory[self._memory[self.pos + 3]] = x * y
                 self.pos += 4
             elif op == Intcode.READ:
                 self._memory[self._memory[self.pos + 1]] = self._input.pop(0)
                 self.pos += 2
             elif op == Intcode.WRITE:
-                x, = self.values(self.pos, 1, modes)
+                x, = self.values(1, modes)
                 self.pos += 2
                 return x
             elif op == Intcode.JUMP_IF_TRUE:
-                x, y = self.values(self.pos, 2, modes)
+                x, y = self.values(2, modes)
                 if x != 0:
                     self.pos = y
                 else:
                     self.pos += 3
             elif op == Intcode.JUMP_IF_FALSE:
-                x, y = self.values(self.pos, 2, modes)
+                x, y = self.values(2, modes)
                 if x == 0:
                     self.pos = y
                 else:
                     self.pos += 3
             elif op == Intcode.LESS:
-                x, y = self.values(self.pos, 2, modes)
+                x, y = self.values(2, modes)
                 self._memory[self._memory[self.pos + 3]] = (1 if x < y else 0)
                 self.pos += 4
             elif op == Intcode.EQUALS:
-                x, y = self.values(self.pos, 2, modes)
+                x, y = self.values(2, modes)
                 self._memory[self._memory[self.pos + 3]] = (1 if x == y else 0)
                 self.pos += 4
             else:
@@ -87,8 +88,7 @@ class Intcode:
         return None
 
     def run(self, inp: Optional[list] = None) -> list:
-        self.reset()
-        self._input = inp[:] if inp else []
+        self.reset(inp)
         output = []
         while (o := self.run_until_output()) is not None:
             output.append(o)
